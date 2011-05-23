9c7e3f89fcb2a70531b82557cbc52e4827a32b27
Laurent Charignon <l.charignon@gmail.com>
2011-05-23 11:14:41 +0200

    minor fixes

--- dpa.c
+++ dpa.c
@@ -15,33 +15,66 @@ int hamming_string(int length, unsigned char * s1,unsigned char * s2);
 int hamming_byte(unsigned char byte1, unsigned char byte2);
 
 
+
+double average_sample(trace_s *a)
+{
+  double total = 0;
+  for (int i = 0 ; i < a->num_samples ; i ++)
+    {    total += a->samples[i];
+      printf("\n%f",a->samples[i]);
+      
+}
+  
+
+ total = total / a->num_samples;
+ return total;
+ 
+}
+
+
+
 void dump_trace_node(trace_s *a)
 {
   for (int k =0;k<8;k++)
     {
       printf("PLAIN TEXT : \n");
-       dump_byte(a->message[k]);
-       printf("\nCIPHERED :\n");
-       dump_byte(a->ciphertext[k]);
-       printf("\nKEY :\n");
-       dump_byte(a->key[k]);
+      dump_byte(a->message[k]);
+      printf("\nCIPHERED :\n");
+      dump_byte(a->ciphertext[k]);
+      printf("\nKEY :\n");
+      dump_byte(a->key[k]);
     }
 }
 
 void dump_byte(unsigned char a)
 {
-       for (int j = 8 ; j!= 0 ; j --)
-	 printf("%d",((a)&(0x1<<j))>>j);
-  
+  for (int j = 8 ; j!= 0 ; j --)
+    printf("%d",((a)&(0x1<<j))>>j);
 }
+
+void dump_string(int length, unsigned char * st)
+{
+    for (int l = 0 ; l < length ; l ++)
+      dump_byte(st[l]);
+    
+}
+
+void dump_string_hexa(int length, unsigned char * st)
+{
+    for (int l = 0 ; l < length ; l ++)
+      printf("%02x",st[l]);
+    
+}
+
+
 
 
 void show_des_result(unsigned char * message, unsigned char * out, unsigned char * key)
 {
-      des_encrypt(message,out ,key);
-      printf("\n RESULT :\n ");
-      for (int l=0;l<8;l++)
-	dump_byte(out[l]);
+  des_encrypt(message,out ,key);
+  printf("\n RESULT :\n ");
+  for (int l=0;l<8;l++)
+    dump_byte(out[l]);
 }
 
 int hamming_byte(unsigned char byte1, unsigned char byte2)
@@ -59,10 +92,7 @@ int hamming_string(int length, unsigned char * s1,unsigned char * s2)
   for (int l = 0 ; l < length ; l ++)
     count += hamming_byte(s1[l],s2[l]);
   return count;
-  
-  
-
-
+ 
 }
 
 
@@ -80,23 +110,23 @@ int main(int argc, char **argv)
   fflush(stdout);
 
   read_traces_from_dir(argv[1]);
- trace_node* current;
+  trace_node* current;
   current = traces_list;
   unsigned char * out = (unsigned char*) malloc(sizeof(unsigned char)*48);
   for (int i = 0 ; i <atoi(argv[2]) ; i ++)
     {
-      printf("TRACE NODE NUMBER %d :\n",i); 
-      dump_trace_node(current->trace);
-      show_des_result(current->trace->message,out ,current->trace->key);
+      printf("\nTRACE NODE NUMBER %d :",i);
+      printf("\n  message : ");
+      dump_string_hexa(8,current->trace->message);
+      
+      
+      printf("\n%f\n",average_sample(current->trace));  
       current = current->next;
-      }
+
+    }
  
 
-  unsigned char a[] = "fffffffffffffff";
-  unsigned char b[]=  "000000000000000";
-  
-  printf("\n %d \n",hamming_string(15,a,b));
-  
+
 
     
   free(out);
